96416e44d298c66e9e56e02717182bc1956c11f7
kbaez <kbaez1990@gmail.com>
2017-03-30 00:15:49 -0300

    add delete

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const port = process.env.PORT || 3001
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+
 app.get('/api/product', (req, res)=> {
 	Product.find({},(err,products) => {
 		if(err) return res.status(500).send({message: `Error al realizar la peticion: ${err}`})
@@ -54,11 +55,27 @@ app.post('/api/product', (req, res)=> {
 })
 
 app.put('/api/product/:productId', (req, res)=> {
+	let productId = req.params.productId
+	let update = req.body
 
+	Product.findByIdAndUpdate(productId, update, (err, productUpdated) =>{
+		if(err) res.status(500).send({message: `Error al salvar en base de datos ${err}`})
+
+		res.status(200).send({product: productUpdated})	
+	})
 })
 
 app.delete('/api/product/:productId', (req, res)=> {
+	let productId = req.params.productId
+	Product.findById(productId, (err, product) => {
+		if(err) return res.status(500).send({message: `Error al borrar el producto: ${err}`})
+		if(!product) return res.status(404).send({message: `El producto no existe`})
 
+		product.remove(err =>{
+			if(err) return res.status(500).send({message: `Error al borrar el producto: ${err}`})
+			res.status(200).send({message: 'El producto ha sido eliminado'})
+		})	
+	})
 })
 
 mongoose.connect('mongodb://localhost:27017/shop',(err, res) => {

294710fd4771baa9efd708e58dcab120af18ba53
kbaez <kbaez1990@gmail.com>
2017-03-31 20:45:57 -0300

    add autentication and user

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const ProductController = require('../controllers/product')
+const auth = require('../middlewares/auth')
 const api = express.Router()
 
 api.get('/product', ProductController.getProducts)
@@ -9,5 +10,8 @@ api.get('/product/:productId', ProductController.getProduct)
 api.post('/product', ProductController.saveProduct)
 api.put('/product/:productId', ProductController.updateProduct)
 api.delete('/product/:productId', ProductController.deleteProduct)
+api.get('/private', function(req, res){
+	res.status(200).send({message: 'Tienes acceso'})
+})
 
 module.exports = api
